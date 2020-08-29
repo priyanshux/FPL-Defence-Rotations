f4f8eb31cd352ee0b0aea22c8062ff57a438671c
Priyanshu <priyanshux@iitkgp.ac.in>
2020-08-30 03:19:18 +0530

    First Finale

--- main.py
+++ main.py
@@ -3,23 +3,20 @@ import itertools
 
 FILENAME = "FPL_Defence_FDR.csv"
 RATINGS = pd.read_csv(FILENAME)
-
-DEF_IN_SQUAD = 5 #Including 2 Goalkeepers
-DEF_IN_XI = 2
-
-# GAMEWEEK = "GW1"
-# RATINGS = pd.read_csv(FILENAME)
-# TOTAL_COMB = list(itertools.combinations(RATINGS[GAMEWEEK], DEF_IN_SQUAD))
-# TOTAL_COMB = [(sorted(x)[:DEF_IN_XI]) for x in TOTAL_COMB]
-# print(TOTAL_COMB)
+DEF_IN_SQUAD = 5 #including two Goalkeepers
+DEF_IN_XI = 3
+STARTING_GW = 1
+ENDING_GW = 11 #GW10 (Increment)
 
 def validCombinations():
-  for i in range(1,39,1):
+  for i in range(STARTING_GW, ENDING_GW, 1):
     GAMEWEEK = "GW{}".format(i)
     TOTAL_COMB = list(itertools.combinations(RATINGS[GAMEWEEK], DEF_IN_SQUAD))
     VALID_COMB = [(sum(sorted(x)[:DEF_IN_XI])) for x in TOTAL_COMB]
     yield(VALID_COMB)
 
 for i in validCombinations():
-  print(i)
+  FINAL = [i[j] + i[j] for j in range(len(i)) if i[j] > 0]
 
+
+print(FINAL)
