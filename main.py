736ba9a2054fc84b4b1087788c7af7d334286714
Priyanshu <priyanshux@iitkgp.ac.in>
2020-08-31 00:51:08 +0530

    main py update

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pandas as pd
 import itertools
+import csv
 
 FILENAME = "FPL_Defence_FDR.csv"
 RATINGS = pd.read_csv(FILENAME)
-DEF_IN_SQUAD = 5 #including two Goalkeepers
-DEF_IN_XI = 3
+DEF_IN_SQUAD = 4 #including two Goalkeepers
+DEF_IN_XI = 2
 STARTING_GW = 1
 ENDING_GW = 11 #GW10 (Increment)
 
-def validCombinations():
-  for i in range(STARTING_GW, ENDING_GW, 1):
-    GAMEWEEK = "GW{}".format(i)
-    TOTAL_COMB = list(itertools.combinations(RATINGS[GAMEWEEK], DEF_IN_SQUAD))
-    VALID_COMB = [(sum(sorted(x)[:DEF_IN_XI])) for x in TOTAL_COMB]
-    yield(VALID_COMB)
+def validCombinations(GAMEWEEK):
+  TOTAL_COMB = list(itertools.combinations(RATINGS[GAMEWEEK], DEF_IN_SQUAD))
+  VALID_COMB = [(sum(sorted(x)[:DEF_IN_XI])) for x in TOTAL_COMB]
+  return(VALID_COMB)
 
-for i in validCombinations():
-  FINAL = [i[j] + i[j] for j in range(len(i)) if i[j] > 0]
+TEAM_COMB = RATINGS.columns
 
 
-print(FINAL)
+for i in range(STARTING_GW, ENDING_GW, 1):
+  GW = "GW{}".format(i)
+  a = validCombinations(GW)
+  # print(len(a))
+  TEAM_COMB = TEAM_COMB.insert(i, a)
+
+
+file = open('GW10.csv', 'w+', newline ='') 
+  
+with file:     
+    write = csv.writer(file) 
+    write.writerows(TEAM_COMB) 
